4d65b8e0ab5efc5a4e87c119545f2e037485c6a4
fruittea-ai <mhmdrosyid19@gmail.com>
2025-11-20 22:16:57 +0700

    edite login activity

--- app/src/main/java/com/proyek/planity/LoginActivity.kt
+++ app/src/main/java/com/proyek/planity/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.proyek.planity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: AppCompatButton
+    private lateinit var tvRegisterLink: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,49 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initViews()
+
+        setupLoginListener()
+
+        setupRegisterListener()
+    }
+
+    private fun initViews() {
+        etEmail = findViewById<EditText>(R.id.etTextEmailAddress)
+        etPassword = findViewById<EditText>(R.id.etTextPassword)
+        btnLogin = findViewById<AppCompatButton>(R.id.btnLogin)
+        tvRegisterLink = findViewById<TextView>(R.id.tvRegisterLink)
+    }
+
+    private fun setupLoginListener() {
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if (email.isEmpty()) {
+                etEmail.error = "Email is required"
+                etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                etPassword.error = "Password is required"
+                etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            Toast.makeText(this, "Login Successful! Email : $email", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            }
+        }
+    private fun setupRegisterListener() {
+        tvRegisterLink.setOnClickListener {
+
         }
     }
 }
