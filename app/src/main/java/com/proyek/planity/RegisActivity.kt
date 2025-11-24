20e5b497bf5607a766f10edd073b8b7b31e1276a
Nadia271205 <tfara@gmail.com>
2025-11-24 11:44:05 +0700

    add register

---
+++ app/src/main/java/com/proyek/planity/RegisActivity.kt
@@ -0,0 +1,36 @@
+package com.proyek.planity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_regis)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val username = findViewById<EditText>(R.id.editTextTextusernameregis)
+        val email = findViewById<EditText>(R.id.editTextTextemailregis)
+        val password = findViewById<EditText>(R.id.editTextTextPassword3)
+        val btnregis = findViewById<Button>(R.id.buttonregis)
+
+        btnregis.setOnClickListener {
+            val email : String = email.text.toString()
+            val password : String = password.text.toString()
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.putExtra("emailregis", email)
+            intent.putExtra("passwordregis", password)
+            startActivity(intent)
+        }
+    }
+}
