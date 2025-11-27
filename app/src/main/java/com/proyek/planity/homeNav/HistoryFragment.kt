dbe49c1de226f7a15b747ec44aa5d6760f4c395d
fruittea-ai <mhmdrosyid19@gmail.com>
2025-11-27 14:38:54 +0700

    buat history fragment

--- app/src/main/java/com/proyek/planity/homeNav/HistoryFragment.kt
+++ app/src/main/java/com/proyek/planity/homeNav/HistoryFragment.kt
@@ -5,18 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.proyek.planity.R
-
+import com.proyek.planity.TaskAdapter
+import com.proyek.planity.TaskViewModel
 
 class HistoryFragment : Fragment() {
+
+    private lateinit var historyAdapter: TaskAdapter
+    private lateinit var viewModel: TaskViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity())[TaskViewModel::class.java]
+
+        val rvHistory = view.findViewById<RecyclerView>(R.id.rvHistory)
+        rvHistory.layoutManager = LinearLayoutManager(context)
+
+        historyAdapter = TaskAdapter(emptyList()) { task ->
+            viewModel.updateTaskStatus(task, false)
+        }
+
+        rvHistory.adapter = historyAdapter
+
+        viewModel.taskList.observe(viewLifecycleOwner) { allTasks ->
+
+            val completedTasks = allTasks.filter { it.isCompleted }
+
+            historyAdapter.updateData(completedTasks)
+        }
+    }
 }
